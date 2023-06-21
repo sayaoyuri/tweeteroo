2d592ca5ffe978785462f081ff97c39afba7b932
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-06-21 16:04:02 -0300

    Add /tweets endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import { users, tweets } from './data.js';
 
 const app = express();
+app.use(cors());
 
-const PORT = 4000;
+app.get('/tweets', (req, res) => {
+  const userList = users.data;
+  const data = tweets.data.slice(-10).reverse().map(tweet => {
+    const user = userList.find(user => user.username == tweet.username); // need to get the tweet user's avatar
+    tweet.avatar = user.avatar;
+
+    return tweet;
+  })
+
+  res.send(data);
+});
+
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const users = {
+  data: []
+};
+
+export const tweets = {
+  data: []
+};
