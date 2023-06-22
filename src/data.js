027341093abb7b3a8e8198b25aa41198a5bc6ff2
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-06-22 08:47:40 -0300

    Running HUB tests

--- src/data.js
+++ src/data.js
@@ -1,40 +1,7 @@
 export const users = {
-  data: [
-    {
-      username: 'bobesponja', 
-      avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-    },
-    {
-      username: 'fakeuser', 
-      avatar: "https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg"
-    },
-    {
-      username: 'usertest', 
-      avatar: "https://hips.hearstapps.com/hmg-prod/images/small-fuffy-dog-breeds-1623362663.jpg?crop=1.00xw:0.753xh;0,0.0719xh&resize=1200:*"
-    },
-    {
-      username: 'teste', 
-      avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-    }
-  ]
+  data: []
 };
+
 export const tweets = {
-  data: [
-    {
-      username: "bobesponja",
-      tweet: "Eu amo hambúrguer de siri!"
-    },
-    {
-      username: "fakeuser",
-      tweet: "Eu amo hambúrguer de carne!"
-    },
-    {
-      username: "usertest",
-      tweet: "Eu amo hambúrguer de frango!"
-    },
-    {
-      username: "teste",
-      tweet: "Eu amo hambúrguer de calabresa!"
-    },
-  ]
+  data: []
 };
